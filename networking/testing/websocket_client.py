9eae044376ccc9c0df07cd32875185c4cb1c1413
Vishwa1011 <imt_2022119@iiitm.ac.in>
2025-01-23 19:55:54 +0530

    Fixed Some Issue

--- networking/testing/websocket_client.py
+++ networking/testing/websocket_client.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def client():
-    uri = "ws://127.0.0.1:8765"  # Replace with your server URI
+    uri = "ws://192.168.69.14:8765"  # Replace with your server URI
     async with websockets.connect(uri) as websocket:
         print("Connected to server")
         while True:
