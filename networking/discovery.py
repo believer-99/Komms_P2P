c125369785eccab04d749a8a0bae581cfb62bf4b
Vishwa1011 <imt_2022119@iiitm.ac.in>
2025-01-29 14:48:22 +0530

    Server code error fixed

--- networking/discovery.py
+++ networking/discovery.py
@@ -1,45 +1,27 @@
 import socket
-import struct
+import json
 
-MULTICAST_GROUP = '224.0.0.1'  # Multicast group address
 DISCOVERY_PORT = 50001  # Port for discovery
 DISCOVERY_MESSAGE = "P2P_DISCOVERY" # The message sent for discovery
-
+# Function for receiving broadcasts
 def receive_broadcasts(peer_list):
-    """
-    Receives multicast discovery messages.
-
-    Args:
-        peer_list (list): List to store discovered peer IP addresses.
-    """
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    # Bind to all interfaces on the specified port
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(('0.0.0.0', DISCOVERY_PORT))
-
-    # Join the multicast group.
-    group = socket.inet_aton(MULTICAST_GROUP)
-    mreq = struct.pack('4sL', group, socket.INADDR_ANY)
-    server_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-
     while True:
         data, addr = server_socket.recvfrom(1024)
         if data.decode() == DISCOVERY_MESSAGE:
-            if addr[0] not in peer_list:
-                print(f"Found peer at {addr[0]}")
-                peer_list.append(addr[0])
+          if addr[0] not in peer_list:
+           print(f"Found peer at {addr[0]}")
+           peer_list.append(addr[0])
 
 
+# Function for sending broadcasts
 def send_broadcasts():
-    """
-    Sends multicast discovery messages.
-    """
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    broadcast_address = ('255.255.255.255', DISCOVERY_PORT) # Send message to all the machines in the local network
     try:
-        client_socket.sendto(DISCOVERY_MESSAGE.encode(), (MULTICAST_GROUP, DISCOVERY_PORT))
+         client_socket.sendto(DISCOVERY_MESSAGE.encode(), broadcast_address)
     except Exception as e:
-        print(f"Error Sending Multicast Message {e}")
-
-    print(f"Broadcasted Discovery Message to {MULTICAST_GROUP}")
+          print(f"Error Sending Broadcast {e}")
+    print("Broadcasted Discovery Message")
